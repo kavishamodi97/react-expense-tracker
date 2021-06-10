0cd310962012964ef8235a07beb1703f8b01b69d
kavishamodi97 <kavishamodi971@gmail.com>
2021-06-10 22:33:17 +0530

    state and event managed by react hooks useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,18 @@
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+import React, { useState } from "react";
 
-const ExpenseItem=(props) => {
+const ExpenseItem = (props) => {
   return (
-    <Card className='expense-item'>
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
